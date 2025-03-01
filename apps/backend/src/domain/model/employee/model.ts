66649dafdac3643b46d12b137e63b9a5e77172e7
nargok <nargok@gmail.com>
2025-03-01 17:12:57 +0900

    prettier for backend

--- apps/backend/src/domain/model/employee/model.ts
+++ apps/backend/src/domain/model/employee/model.ts
@@ -1,18 +1,18 @@
-type EmployeeStatus = "active" | "inactive";
+type EmployeeStatus = 'active' | 'inactive';
 
 export class Employee {
-    constructor(
-        public id: number,
-        public name: string,
-        public email: string,
-        public joinDate: Date,
-        public status: EmployeeStatus,
-    ) {}
+  constructor(
+    public id: number,
+    public name: string,
+    public email: string,
+    public joinDate: Date,
+    public status: EmployeeStatus,
+  ) {}
 
-    public get isNew(): boolean {
-        const thirtyDaysAgo = new Date();
-        thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+  public get isNew(): boolean {
+    const thirtyDaysAgo = new Date();
+    thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
 
-        return this.joinDate > thirtyDaysAgo;
-    }
+    return this.joinDate > thirtyDaysAgo;
+  }
 }
